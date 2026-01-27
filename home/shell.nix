8eaaeaa52ea7295048753d0a554bed968b1e4b97
foxtrottt <dawn.spinal795@passmail.net>
2026-01-26 23:30:57 -0500

    Styled fastfetch

--- home/shell.nix
+++ home/shell.nix
@@ -9,4 +9,10 @@
       rebuild = "sudo nixos-rebuild switch --flake /home/foxtrot/olympus-nixos/";
     };
   };
+
+   xdg.configFile = {
+    "fastfetch/config.jsonc".source = ./config/fastfetch.jsonc;
+    #"noctalia/settings.json".force = true;
+  };
+
 }
