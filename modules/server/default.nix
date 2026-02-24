f2d3eff64157e186fda6d0c615fe12f547083cd4
foxtrottt <dawn.spinal795@passmail.net>
2026-02-23 19:46:39 -0500

    Adjusted services and made BIOS change to help fix random shutdown/restarting(?) issue

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -4,15 +4,15 @@
     ./services/jellyfin.nix
     ./services/qbittorrent.nix
     ../functions.nix
-    ./services/samba.nix
+    #./services/samba.nix
     ./nvidia.nix
     ./packages.nix
     ./services/frigate.nix
     ./services/adguardhome.nix
     ./services/navidrome.nix
-    ../xfce/default.nix
-    ./services/x2go.nix
-    ./services/dawarich.nix
+    #../xfce/default.nix
+    #./services/x2go.nix
+    #./services/dawarich.nix
     ./services/avahi.nix
     ./services/minecraft.nix
     ./services/homeassistant.nix
