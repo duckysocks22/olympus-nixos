25370e5c8b2f3950dfba716e606a9267d13acee8
foxtrottt <dawn.spinal795@passmail.net>
2026-03-02 13:17:20 -0500

    Added 'lastfm' api key to 'navidrome'

--- modules/server/services/navidrome.nix
+++ modules/server/services/navidrome.nix
@@ -8,6 +8,9 @@
       MusicFolder = "/media/hdd1/music";
       EnableSharing = true;
       openFirewall = true;
+      ND_LASTFM_APIKEY = "81dd1933879c04c5b933470fde4c3f7f";
+      # No i do not care im sharing this secret
+      ND_LASTFM_SECRET = "4ed0d1c9dbf1dc1a6a560dcf88c8f0e3";
     };
   };
   systemd.services.navidrome.serviceConfig.ProtectHome = lib.mkForce "read-only";
