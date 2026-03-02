f68c5807eea0ea5b62a7eafc1be64c4fe250dd0c
foxtrottt <dawn.spinal795@passmail.net>
2026-03-01 21:48:34 -0500

    Added 'asunder' to 'programs/download'

--- home/programs/download.nix
+++ home/programs/download.nix
@@ -3,6 +3,7 @@
   home.packages = (with pkgs; [
     nicotine-plus
     qbittorrent
+    asunder
   ]) ++ (with inputs.luxxy-pkgs.packages.${pkgs.system}; [
     (jdownloader.override {
       darkTheme = true;
