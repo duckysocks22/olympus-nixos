55f019c5900bbaa8b0eaf772681cb62a8c178407
foxtrottt <dawn.spinal795@passmail.net>
2026-03-07 16:18:00 -0500

    Disabled 'statech_industry' server and added 'atm10' server

--- modules/server/services/game-servers/minecraft.nix
+++ modules/server/services/game-servers/minecraft.nix
@@ -1,6 +1,6 @@
 { util, pkgs, lib, ...}:
 {
-  systemd.services.minecraft-statech-industry = util.functions.mkSimpleService {
+  /* systemd.services.minecraft-statech-industry = util.functions.mkSimpleService {
       description = "Statech Industry Minecraft Server";
       ExecStart = "${pkgs.writeShellScript "start.sh check" ''
         set -x
@@ -11,5 +11,18 @@
         fi
       ''}";
       user = "server";
+  };*/
+
+  systemd.services.minecraft-atm10 = util.functions.mkSimpleService {
+    description = "All The Mods 10 Minecraft Server";
+    ExecStart = "${pkgs.writeShellScript "start.sh check" ''
+      set -x
+      PATH="${lib.makeBinPath [ pkgs.jre ]}:PATH"
+      if [ -d /media/hdd1/game-servers/minecraft/atm10/ ]; then
+        cd /media/hdd1/game-servers/minecraft/atm10
+        java -server -Xms6G -Xmx12G -jar server.jar
+      fi
+    ''}";
+    user = "server";
   };
 }
