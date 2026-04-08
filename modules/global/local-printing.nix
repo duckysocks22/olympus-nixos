76abb3f8255172a47d459ca08bbbd4c77aec3530
foxtrottt <dawn.spinal795@passmail.net>
2026-04-08 17:58:50 -0400

    printing :3

--- modules/global/local-printing.nix
+++ modules/global/local-printing.nix
@@ -1,20 +1,5 @@
 { pkgs, ...}:
 {
   services.printing.enable = true;
-  services.printing.drivers = with pkgs; [ canon-cups-ufr2 ];
-
-  hardware.printers = {
-    ensurePrinters = [
-      {
-        name = "muse";
-        location = "home";
-        deviceUri = "http://172.17.100.1:6831/printers/muse";
-        model = "drv:///sample.drv/generic.ppd";
-        ppdOptions = {
-          PageSize = "A4";
-        };
-      }
-    ];
-    ensureDefaultPrinter = "muse";
-  };
+  services.printing.drivers = with pkgs; [ cups-filters cups-browsed canon-cups-ufr2 ];
 }
