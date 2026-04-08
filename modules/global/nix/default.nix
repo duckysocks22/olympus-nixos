7c483c024a1fdb37c3ec57dc232b51e3e3721747
foxtrottt <dawn.spinal795@passmail.net>
2026-04-07 20:45:25 -0400

    Setting 'cache.puppygirls.net' as substituter for all hosts (Fixed)

--- modules/global/nix/default.nix
+++ modules/global/nix/default.nix
@@ -5,7 +5,6 @@
       auto-optimise-store = true;
       keep-derivations = true;
       keep-outputs = true;
-<<<<<<< HEAD
 
       substituters = [
         "https://cache.puppygirls.net/main"
@@ -13,8 +12,6 @@
       trusted-public-keys = [
         "main:t80OgdIBHuIkHqncmTFtABUsciAdJC/HsstSck6t4p0="
       ];
-=======
->>>>>>> a8e1274ffc9baf2a990cf9be6217ff151167090e
     };
     nixPath = lib.mapAttrsToList (n: v: "${n}=flake:${n}") inputs;
     registry = lib.mapAttrs (n: v: { flake = v; }) inputs;
