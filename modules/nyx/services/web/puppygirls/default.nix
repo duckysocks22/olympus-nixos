2b84567c5fe86d39209fbcd6a8976683223fce83
foxtrottt <dawn.spinal795@passmail.net>
2026-04-11 01:17:44 -0400

    Started proper work on 'www.puppygirls.net' website

--- modules/nyx/services/web/puppygirls/default.nix
+++ modules/nyx/services/web/puppygirls/default.nix
@@ -4,7 +4,7 @@ pkgs.buildNpmPackage (self: {
   pname = "puppygirls";
   version = "0.1.0";
   src = ./.;
-  npmDepsHash = "sha256-iXovAm85d0VsON2DidB9XPnz4HTI1sLUPJ0K2VSeyaM=";
+  npmDepsHash = "sha256-Z0zGQUvfPWA7KHwBBMUpwqNWM0YPii5HjMeGXB77Zio=";
 
   buildPhase = ''
     npm run build
