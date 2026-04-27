f2f7ddee6261c0721a7bfd1ed564f23bedaf2e9c
foxtrottt <dawn.spinal795@passmail.net>
2026-04-27 15:43:53 -0400

    Removed 'yt-dlp' cronjobs because audiobookshelf has a built in way to fetch and automate getting epsidoes :xdf:

--- modules/nyx/services/yt-dlp/default.nix
+++ modules/nyx/services/yt-dlp/default.nix
@@ -4,13 +4,4 @@
   environment.systemPackages = with pkgs; [
     python312Packages.yt-dlp
   ];
-
-  services.cron = {
-    enable = true;
-    systemCronJobs = [
-      "0 3 */1 * *      server          yt-dlp -P /media/hdd1/audio/podcasts/Rotten_Mango/ -x --audio-format flac --audio-quality 0 --sponsorblock-remove default --embed-thumbnail --download-archive /media/hdd1/audio/podcasts/Rotten_Mango/archive.txt https://www.youtube.com/watch?v=-Sd9LX3aGFw&list=PLsDkDDndB1qsQToK8Ec0E0F8x6iLHWv5O"
-      "0 3 */1 * *      server          yt-dlp -P /meedia/hdd1/audio/podcasts/Financial_Audit/ -x --audio-format flac --audio-quality 0 --sponsorblock-remove default --embed-thumbnail --download-archive /media/hdd1/audio/podcasts/Financial_Audit/archive.txt https://www.youtube.com/watch?v=8u7HqZJQHDM&list=PLzJVLNWKVr6ksDjycE7NpSptOlcaEP-jQ"
-      "0 3 */1 * *      server          yt-dlp -P /media/hdd1/audio/podcasts/The_WAN_Show/ -x --audio-format flac --audio-quality 0 --sponsorblock-remove default --embed-thumbnail --download-archive /media/hdd1/audio/podcasts/The_WAN_Show/archive.txt https://www.youtube.com/watch?v=dm6aRONewxk&list=PL8mG-RkN2uTw7PhlnAr4pZZz2QubIbujH&index=1"
-    ];
-  };
 }
