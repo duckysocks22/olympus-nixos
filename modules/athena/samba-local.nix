9ac0a800a8e5152779a4ac9ea33f72ca19bfbabf
foxtrottt <dawn.spinal795@passmail.net>
2026-05-04 14:00:20 -0400

    Updated 'samba-local' to not output into terminal on rebuild it mound fails 'I.E away from home'

--- modules/athena/samba-local.nix
+++ modules/athena/samba-local.nix
@@ -4,30 +4,29 @@
 
   systemd.mounts = [
     {
-      enable = true;
       description = "Olympus Shared SMB mount";
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
-      wantedBy = [ "multi-user.target" ];
-
       what = "//172.17.100.1/shared";
       where = "/media/olympus/shared";
-      options = "credentials=${config.sops.secrets."samba/local".path},uid=1000,gid=100,file_mode=0664,dir_mode=0775,nofail";
+      options = "credentials=${config.sops.secrets."samba/local".path},uid=1000,gid=100,file_mode=0664,dir_mode=0775,x-systemd.automount,noauto,x-systemd.device-timeout=5s";
       type = "cifs";
-      mountConfig.TimeoutSec = 15;
     }
     {
-      enable = true;
       description = "Olympus Private SMB mount";
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
-      wantedBy = [ "multi-user.target" ];
-
       what = "//172.17.100.1/private";
       where = "/media/olympus/private";
-      options = "credentials=${config.sops.secrets."samba/local".path},uid=1000,gid=100,file_mode=0664,dir_mode=0775,nofail";
+      options = "credentials=${config.sops.secrets."samba/local".path},uid=1000,gid=100,file_mode=0664,dir_mode=0775,x-systemd.automount,noauto,x-systemd.device-timeout=5s";
       type = "cifs";
-      mountConfig.TimeoutSec = 15;
+    }
+  ];
+
+  systemd.automounts = [
+    {
+      wantedBy = [ "multi-user.target" ];
+      where = "/media/olympus/shared";
+    }
+    {
+      wantedBy = [ "multi-user.target" ];
+      where = "/media/olympus/private";
     }
   ];
 
