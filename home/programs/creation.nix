693c215dc552ade895d6e8c683019cc2c2922390
foxtrottt <dawn.spinal795@passmail.net>
2026-07-16 12:49:44 -0400

    home: Added 'obs-studio' to package list

--- home/programs/creation.nix
+++ home/programs/creation.nix
@@ -11,4 +11,17 @@
     pkgs.rawtherapee
     pkgs.ansel
   ];
+
+  programs.obs-studio = {
+    enable = true;
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
 }
