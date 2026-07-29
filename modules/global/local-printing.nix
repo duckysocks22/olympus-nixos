302ba9594ebd416a565d7585ba205e49f954b07b
foxtrottt <code@olympus.moe>
2026-07-28 22:46:02 -0400

    fix(printer): Declaritivesly added Canon MF270 network printer

--- modules/global/local-printing.nix
+++ modules/global/local-printing.nix
@@ -6,4 +6,14 @@
     cups-browsed
     canon-cups-ufr2
   ];
+
+  hardware.printers.ensurePrinters = [
+    {
+      name = "Canon-MF270";
+      description = "Canon MF270 Series";
+      deviceUri = "dnssd://Canon%20MF270%20Series._ipp._tcp.local/?uuid=6d4ff0ce-6b11-11d8-8020-6c3c7c3bf858";
+      model = "CNRCUPSMF270ZK.ppd";
+    }
+  ];
+  hardware.printers.ensureDefaultPrinter = "Canon-MF270";
 }
