2f86be85df170db40f0997432bca5015b60f6f5f
foxtrottt <code@olympus.moe>
2026-08-03 11:54:08 -0400

    feat(nh): Updated extra args for 'nh clean'

--- modules/global/nix/default.nix
+++ modules/global/nix/default.nix
@@ -36,7 +36,7 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    clean.extraArgs = "--keep-since 7d --keep 3";
     flake = "/home/$(whoami)/olympus-nixos";
   };
 }
