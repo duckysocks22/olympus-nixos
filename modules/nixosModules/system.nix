2846ff52f69292221c8b3b6964138ba9ed53b636
foxtrottt <code@olympus.moe>
2026-09-08 00:17:57 -0400

    feat(system): Added 'quiet' kerel param
    feat(system): Added 'splash' kernel param

--- modules/nixosModules/system.nix
+++ modules/nixosModules/system.nix
@@ -100,8 +100,8 @@
       kernelParams = [
         "amd_iommu=on"
         "amd_pstate=active"
-        "rd.udev.log_level=3"
-        "rd.systemd.show_status=auto"
+        "quiet"
+        "splash"
       ];
       loader = {
         limine = {
