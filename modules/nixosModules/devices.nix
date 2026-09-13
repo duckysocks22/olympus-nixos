0436481a80b6782e6fa2e6191046e4a9240fcc76
foxtrottt <code@olympus.moe>
2026-09-13 12:18:50 -0400

    fix(printer): Fixed Canon-M270 printer not being reachable

--- modules/nixosModules/devices.nix
+++ modules/nixosModules/devices.nix
@@ -14,7 +14,7 @@
       {
         name = "Canon-MF270";
         description = "Canon MF270 Series";
-        deviceUri = "dnssd://Canon%20MF270%20Series._ipp._tcp.local/?uuid=6d4ff0ce-6b11-11d8-8020-6c3c7c3bf858";
+        deviceUri = "ipp://172.17.110.1/ipp/print";
         model = "CNRCUPSMF270ZK.ppd";
       }
     ];
