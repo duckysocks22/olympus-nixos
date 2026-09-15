5ef308ce7fe55dfc794b886f07c9a91422c1a05d
foxtrottt <code@olympus.moe>
2026-09-15 17:02:05 -0400

    fix(remotebuild): Updated 'nyx-nixos' definition

--- modules/nixosModules/distributedBuilds.nix
+++ modules/nixosModules/distributedBuilds.nix
@@ -42,6 +42,7 @@
         "172.17.100.1"
         "192.168.10.253"
         "nyx-nixos"
+        "ssh.olympus.moe"
       ];
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEeEqqcPRubu6LqVhSZQY63rv0ALqn8OY1UuLCXB2wfd";
     };
