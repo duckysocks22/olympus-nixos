4928bcc83a60a27352d613ceb76414360fbc2759
foxtrottt <code@olympus.moe>
2026-09-17 05:46:28 -0400

    fix(buildClient): Updated 'nyx-nixos' definition

--- modules/nixosModules/distributedBuilds.nix
+++ modules/nixosModules/distributedBuilds.nix
@@ -8,7 +8,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMFCqhCYgJNuZ0+3oJFFmEjmUNSBPhLSzZfuHWjY2ivc root@ariadne-nixos"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVF7dMsquwXmzdfKtFuD5KJ7SLeftFhl5Ezh1Rf0Aej root@dionysus-nixos"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3YO5pY8RdhIQpeTgwYUuKMoW3yEccvLVkG+SfFZ/47 root@aether-nixos"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0iFfMsPQAbz7QOqgBnZQsJPjVXXq9djMm23+2mnETB root@aether-nixos"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7f9ImZW+fkfzIxW9ZVfcjiUE5NUN+qnYlkpk+mr2F3 root@circe-nixos"
       ];
     };
@@ -23,7 +23,7 @@
 
     nix.buildMachines = [
       {
-        hostName = "nyx-nixos";
+        hostName = "192.168.10.253";
         sshUser = "remotebuild";
         sshKey = "/etc/ssh/ssh_host_ed25519_key";
         systems = [ "x86_64-linux" ];
